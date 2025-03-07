7f1d59f51faa7c3a7bf5c48e63e7d764e041b551
altomizawa <alyssontomizawa@hotmail.com>
2025-03-07 17:10:25 -0300

    quickfix: change order of foundations

--- app/components/TwoFiveTwoDescription.jsx
+++ app/components/TwoFiveTwoDescription.jsx
@@ -8,9 +8,9 @@ const TwoFiveTwoDescription = ({ setIsCounterScreen, category, currentWod, handl
     <div className='mt-4 w-full text-center flex flex-col items-center gap-4'>
       <h2 className='text-3xl font-bold'>25.2</h2>
       <div className='space-x-4 text-sm'>
+        <button onClick={()=>{handleCategoryChange('foundations')}} className={`border-[1px] rounded-md w-min-24 py-2 px-2 cursor-pointer ${category==='foundations' ? 'bg-white text-black border-none' : ''}`}>FOUNDATIONS</button>
         <button onClick={()=>{handleCategoryChange('scaled')}} className={`border-[1px] rounded-md w-24 py-2 cursor-pointer ${category==='scaled' ? 'bg-white text-black border-none' : ''}`}>SCALED</button>
         <button onClick={()=>{handleCategoryChange('rx')}} className={`border-[1px] rounded-md w-24 py-2 cursor-pointer ${category==='rx' ? 'bg-white text-black border-none' : ''}`}>RX</button>
-        <button onClick={()=>{handleCategoryChange('foundations')}} className={`border-[1px] rounded-md w-min-24 py-2 px-2 cursor-pointer ${category==='foundations' ? 'bg-white text-black border-none' : ''}`}>FOUNDATIONS</button>
       </div>
       <div className='mt-4'>
         <p>For Time:</p>
